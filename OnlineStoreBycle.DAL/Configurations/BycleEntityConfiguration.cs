fedcbc801d352f0230fff47a453a4ce3991ba7a5
Kamal Bayramov <kamal44.00@mail.ru>
2024-03-12 16:55:08 +0700

    Добавил таблицы Отзывы, Размер Рамы, Бренд Велосипеда

---
+++ OnlineStoreBycle.DAL/Configurations/BycleEntityConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OnlineStoreBycle.DAL.Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineStoreBycle.DAL.Configurations
+{
+    internal sealed class BycleEntityConfiguration : IEntityTypeConfiguration<BycleEntity>
+    {
+        public void Configure(EntityTypeBuilder<BycleEntity> builder)
+        {
+            builder.HasMany(x => x.Reviews)
+                .WithOne(x => x.Bycle)
+                .HasForeignKey(x => x.BycleId);
+        }
+    }
+}
---
+++ OnlineStoreBycle.DAL/Entites/ReviewsEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineStoreBycle.DAL.Entites
+{
+    internal sealed class ReviewsEntity
+    {
+        public int Id { get; set; }
+        public string Review {  get; set; }=string.Empty;
+        public int BycleId {  get; set; }
+        public BycleEntity Bycle { get; set; } = new();
+    }
+}
